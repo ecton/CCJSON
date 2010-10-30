04d055f0bd3daa991487d5ca3b9bfe1d0828b1a6
Jonathan Johnson <jon@nilobject.com>
2010-10-30 11:42:42 -0500

    Increased parsing speed greatly by removing autorelease calls, optimizing string parsing by minimizing buffer resizes, and localizing the offest indicies.

--- CCJSONParser.m
+++ CCJSONParser.m
@@ -30,8 +30,8 @@
 #import "CCJSONParser.h"
 
 #define SkipWhitespace(offset, json, jsonLength) \
-		while (*offset < jsonLength && (json[*offset] == '\t' || json[*offset] == ' ' || json[*offset] == '\r' || json[*offset] == '\n')) { \
-			*offset += 1; \
+		while (offset < jsonLength && (json[offset] == '\t' || json[offset] == ' ' || json[offset] == '\r' || json[offset] == '\n')) { \
+			offset += 1; \
 		}
 		
 #define xtod(c) ((c) >= '0' && (c) <= '9' ? (c) - '0' : ((c) >= 'A' && (c) <= 'F' ? (c) + 10 - 'A' : ((c) >= 'a' && (c) <= 'f' ? (c) + 10 - 'a' : 0)))
@@ -40,201 +40,225 @@
 	if (outError) { \
 		*outError = [NSError errorWithDomain:@"cocoaconjuror.cc" code:1 userInfo:[NSDictionary dictionaryWithObjectsAndKeys: message, @"message", [NSNumber numberWithInt:offset], @"offset", nil]]; \
 	}\
+	*inOutOffset = offset;\
 	return nil;}
 
 
 @implementation CCJSONParser
 
-static id ParseJSONObject(int *offset, unichar *json, int jsonLength, BOOL useNSNull, NSError **outError) {
+static id ParseJSONObject(int *inOutOffset, unichar *json, int jsonLength, BOOL useNSNull, NSError **outError) {
 	// Skip any whitespace
-	while (json[*offset] == '\t' || json[*offset] == ' ' || json[*offset] == '\r' || json[*offset] == '\n') {
-		*offset += 1;
+	int offset = *inOutOffset;
+	while (json[offset] == '\t' || json[offset] == ' ' || json[offset] == '\r' || json[offset] == '\n') {
+		offset += 1;
 	}
-	if (*offset < jsonLength) {
-		switch (json[*offset]) {
+	if (offset < jsonLength) {
+		switch (json[offset]) {
 			case '{': { // Object
-				*offset += 1;
-				NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+				offset += 1;
+				NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithCapacity:16];
 				SkipWhitespace(offset, json, jsonLength);
-				if (*offset < jsonLength && json[*offset] != '}') {
+				if (offset < jsonLength && json[offset] != '}') {
 					while (true) {
 						SkipWhitespace(offset, json, jsonLength);
-						id key = ParseJSONObject(offset, json, jsonLength, useNSNull, outError);
+						*inOutOffset = offset;
+						id key = ParseJSONObject(inOutOffset, json, jsonLength, useNSNull, outError);
 						if (outError && *outError) return nil;
+						offset = *inOutOffset;
 						SkipWhitespace(offset, json, jsonLength);
-						if (*offset >= jsonLength || json[*offset] != ':') PARSE_ERROR(@"Expected : for object value", *offset, outError);
-						*offset += 1;
+						if (offset >= jsonLength || json[offset] != ':') PARSE_ERROR(@"Expected : for object value", offset, outError);
+						offset += 1;
 						SkipWhitespace(offset, json, jsonLength);
-						id value = ParseJSONObject(offset, json, jsonLength, useNSNull, outError);
+						*inOutOffset = offset;
+						id value = ParseJSONObject(inOutOffset, json, jsonLength, useNSNull, outError);
 						if (outError && *outError) return nil;
+						offset = *inOutOffset;
 						if (value) {
 							[dict setObject:value forKey:key];
+							[value release];
 						}
-						if (*offset >= jsonLength || json[*offset] != ',') break;
-						*offset += 1;
+						[key release];
+						if (offset >= jsonLength || json[offset] != ',') break;
+						offset += 1;
 					}
 					SkipWhitespace(offset, json, jsonLength);
 				}
-				if (*offset >= jsonLength || json[*offset] != '}') PARSE_ERROR(@"Expected } to close object", *offset, outError);
-				*offset += 1;
+				if (offset >= jsonLength || json[offset] != '}') PARSE_ERROR(@"Expected } to close object", offset, outError);
+				offset += 1;
+				*inOutOffset = offset;
 				return dict;
 			} break;
 			
 			case '[': { // Array
-				*offset += 1;
-				NSMutableArray *array = [NSMutableArray array];
+				offset += 1;
+				NSMutableArray *array = [[NSMutableArray alloc] initWithCapacity:16];
 				SkipWhitespace(offset, json, jsonLength);
-				if (*offset < jsonLength && json[*offset] != ']') {
+				if (offset < jsonLength && json[offset] != ']') {
 					while (true) {
 						SkipWhitespace(offset, json, jsonLength);
 						
-						id value = ParseJSONObject(offset, json, jsonLength, useNSNull, outError);
+						*inOutOffset = offset;
+						id value = ParseJSONObject(inOutOffset, json, jsonLength, useNSNull, outError);
 						if (outError && *outError) return nil;
+						offset = *inOutOffset;
 						[array addObject:value];
-						if (*offset >= jsonLength || json[*offset] != ',') break;
-						*offset += 1;
+						[value release];
+						if (offset >= jsonLength || json[offset] != ',') break;
+						offset += 1;
 					}
 					SkipWhitespace(offset, json, jsonLength);
 				}
-				if (*offset >= jsonLength || json[*offset] != ']') PARSE_ERROR(@"Expected ] to close array", *offset, outError);
-				*offset += 1;
+				if (offset >= jsonLength || json[offset] != ']') PARSE_ERROR(@"Expected ] to close array", offset, outError);
+				offset += 1;
+				*inOutOffset = offset;
 				return array;
 			} break;
 			
 			case '"': { // String
-				*offset += 1;
-				NSMutableString *str = [NSMutableString string];
-				while (*offset < jsonLength && json[*offset] != '"') {
-					unichar ch = json[*offset];
-					*offset += 1;
+				offset += 1;
+				NSString *str = nil;
+				unichar stringBuffer[1024];
+				int uncommittedLength = 0;
+				while (offset < jsonLength && json[offset] != '"') {
+					unichar ch = json[offset];
+					offset += 1;
 					if (ch == '\\') {
-						if (*offset >= jsonLength) return nil;
-						ch = json[*offset];
-						*offset += 1;
+						if (offset >= jsonLength) PARSE_ERROR(@"Expected escape char, but got EOF", offset, outError);
+						ch = json[offset];
+						offset += 1;
 						switch (ch) {
-							case '\\':	CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case '"':	ch = '"'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case '/':	ch = '/'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case 'b':	ch = '\b'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case 'f':	ch = '\f'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case 'n':	ch = '\n'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case 'r':	ch = '\r'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
-							case 't':	ch = '\t'; CFStringAppendCharacters((CFMutableStringRef)str, (UniChar *)&ch, 1); break;
+							case '\\': stringBuffer[uncommittedLength++] = ch; break;
+							case '"':	ch = '"'; stringBuffer[uncommittedLength++] = ch; break;
+							case '/':	ch = '/'; stringBuffer[uncommittedLength++] = ch; break;
+							case 'b':	ch = '\b'; stringBuffer[uncommittedLength++] = ch; break;
+							case 'f':	ch = '\f'; stringBuffer[uncommittedLength++] = ch; break;
+							case 'n':	ch = '\n'; stringBuffer[uncommittedLength++] = ch; break;
+							case 'r':	ch = '\r'; stringBuffer[uncommittedLength++] = ch; break;
+							case 't':	ch = '\t'; stringBuffer[uncommittedLength++] = ch; break;
 							case 'u':	
-								if (*offset + 4 >= jsonLength) PARSE_ERROR(@"Expected 4 hex chars for unicode char", *offset, outError);
-								ch = xtod(json[*offset]) << 12 | xtod(json[*offset + 1]) << 8 | xtod(json[*offset + 2]) << 4 | xtod(json[*offset + 3]);
-								*offset += 4;
-								CFStringAppendCharacters((CFMutableStringRef)str, &ch, 1); 
+								if (offset + 4 >= jsonLength) PARSE_ERROR(@"Expected 4 hex chars for unicode char", offset, outError);
+								ch = xtod(json[offset]) << 12 | xtod(json[offset + 1]) << 8 | xtod(json[offset + 2]) << 4 | xtod(json[offset + 3]);
+								offset += 4;
+								stringBuffer[uncommittedLength++] = ch;
 							break;
 						}
 					} else {
-						CFStringAppendCharacters((CFMutableStringRef)str, &ch, 1);
+						stringBuffer[uncommittedLength++] = ch;
+					}
+					if (uncommittedLength == 1024) {
+						if (!str) {
+							// If we've already consumed 1k, it could be much longer -- let's allocate more space to avoid another resize
+							str = [[NSMutableString alloc] initWithCapacity:4096];
+						}
+						CFStringAppendCharacters((CFMutableStringRef)str, stringBuffer, uncommittedLength);
+						uncommittedLength = 0;
 					}
 				}
-				if (*offset >= jsonLength || json[*offset] != '"') PARSE_ERROR(@"Expected closing quote of string", *offset, outError);
-				*offset += 1;
+				if (uncommittedLength > 0) {
+					if (!str) {
+						str = [[NSString alloc] initWithCharacters:stringBuffer length:uncommittedLength];
+					} else {
+						CFStringAppendCharacters((CFMutableStringRef)str, stringBuffer, uncommittedLength);
+					}
+				}
+				if (offset >= jsonLength || json[offset] != '"') PARSE_ERROR(@"Expected closing quote of string", offset, outError);
+				offset += 1;
+				*inOutOffset = offset;
 				return str;
 			} break; 
 			
 			case 't': { // true
-				if (*offset + 4 < jsonLength && json[*offset + 1] == 'r' && json[*offset + 2] == 'u' && json[*offset + 3] == 'e') {
-					*offset += 4;
-					return [NSNumber numberWithBool:YES];
+				if (offset + 4 < jsonLength && json[offset + 1] == 'r' && json[offset + 2] == 'u' && json[offset + 3] == 'e') {
+					offset += 4;
+					*inOutOffset = offset;
+					return [[NSNumber alloc] initWithBool:YES];
 				} else {
-					PARSE_ERROR(@"Unknown value", *offset, outError);
+					PARSE_ERROR(@"Unknown value", offset, outError);
 				}
 			} break; 
 			
 			case 'f': { // false
-				if (*offset + 5 < jsonLength && json[*offset + 1] == 'a' && json[*offset + 2] == 'l' && json[*offset + 3] == 's' && json[*offset + 4] == 'e') {
-					*offset += 5;
-					return [NSNumber numberWithBool:NO];
+				if (offset + 5 < jsonLength && json[offset + 1] == 'a' && json[offset + 2] == 'l' && json[offset + 3] == 's' && json[offset + 4] == 'e') {
+					offset += 5;
+					*inOutOffset = offset;
+					return [[NSNumber alloc] initWithBool:NO];
 				} else {
-					PARSE_ERROR(@"Unknown value", *offset, outError);
+					PARSE_ERROR(@"Unknown value", offset, outError);
 				}
 			} break; 
 			
 			case 'n': { // null
-				if (*offset + 4 < jsonLength && json[*offset + 1] == 'u' && json[*offset + 2] == 'l' && json[*offset + 3] == 'l') {
-					*offset += 4;
+				if (offset + 4 < jsonLength && json[offset + 1] == 'u' && json[offset + 2] == 'l' && json[offset + 3] == 'l') {
+					offset += 4;
+					*inOutOffset = offset;
 					if (useNSNull) {
-						return [NSNull null];
+						return [[NSNull null] retain];
 					}
 					return NULL;
 				} else {
-					PARSE_ERROR(@"Unknown value", *offset, outError);
+					PARSE_ERROR(@"Unknown value", offset, outError);
 				}
 			} break; 
 			
 			default: { // Number or bad stuff
 				BOOL valid = NO;
-				BOOL floatingPoint = NO;
-				long long value = 0;
-				double doubleValue = 0;
+				double value = 0;
 				BOOL neg = NO;
-				if (json[*offset] == '-') {
+				if (json[offset] == '-') {
 					neg = YES;
 					valid = YES;
-					*offset += 1;
+					offset += 1;
 				}
 				
-				while (*offset < jsonLength && json[*offset] >= '0' && json[*offset] <= '9') {
-					value = value * 10 + json[*offset] - '0';
-					doubleValue = doubleValue * 10 + json[*offset] - '0';
+				while (offset < jsonLength && json[offset] >= '0' && json[offset] <= '9') {
+					value = value * 10 + json[offset] - '0';
 					valid = YES;
-					*offset += 1;
+					offset += 1;
 				}
 				
-				if (*offset < jsonLength && json[*offset] == '.') {
-					floatingPoint = YES;
-					*offset += 1;
+				if (offset < jsonLength && json[offset] == '.') {
+					offset += 1;
 					valid = YES;
 					double place = 10;					
-					while (*offset < jsonLength && json[*offset] >= '0' && json[*offset] <= '9') {
-						doubleValue = doubleValue + (double)(json[*offset] - '0') / place;
+					while (offset < jsonLength && json[offset] >= '0' && json[offset] <= '9') {
+						value = value + (double)(json[offset] - '0') / place;
 						place *= 10;
-						*offset += 1;
+						offset += 1;
 					}
 				}
 				
-				if (*offset < jsonLength && (json[*offset] == 'e' || json[*offset] == 'E')) {
-					*offset += 1;
-					floatingPoint = YES;
+				if (offset < jsonLength && (json[offset] == 'e' || json[offset] == 'E')) {
+					offset += 1;
 					valid = YES;
 					BOOL posExp = YES;
-					if (*offset < jsonLength && json[*offset] == '+') {
-						*offset += 1;
-					} else if (*offset < jsonLength && json[*offset] == '+') {
+					if (offset < jsonLength && json[offset] == '+') {
+						offset += 1;
+					} else if (offset < jsonLength && json[offset] == '+') {
 						posExp = NO;
-						*offset += 1;
+						offset += 1;
 					}
 					
 					double exp = 0;
-					while (*offset < jsonLength && json[*offset] >= '0' && json[*offset] <= '9') {
-						exp = exp * 10 + json[*offset] - '0';
-						*offset += 1;
+					while (offset < jsonLength && json[offset] >= '0' && json[offset] <= '9') {
+						exp = exp * 10 + json[offset] - '0';
+						offset += 1;
 					}
 					
-					doubleValue = doubleValue * pow(10, posExp ? exp : -exp);
+					value = value * pow(10, posExp ? exp : -exp);
 				}
 				
 				if (!valid) {
-					PARSE_ERROR(@"Invalid data", *offset, outError);
+					PARSE_ERROR(@"Invalid data", offset, outError);
 				}
 				
-				if (neg) {
-					value = -value;
-					doubleValue = -doubleValue;
-				}
+				if (neg) value = -value;
 				
-				if (floatingPoint) {
-					return [NSNumber numberWithDouble:doubleValue];
-				} else {
-					return [NSNumber numberWithLongLong:value];
-				}
+				*inOutOffset = offset;
+				return [[NSNumber alloc] initWithDouble:value];
 			} break; 
 		}
 	}
+	*inOutOffset = offset;
 	return nil;
 }
 
@@ -256,7 +280,7 @@ static id ParseJSONObject(int *offset, unichar *json, int jsonLength, BOOL useNS
 	}
 	
 	free(json);
-	return obj;
+	return [obj autorelease];
 }
 
 @end
